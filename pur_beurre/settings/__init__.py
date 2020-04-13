e65bdb8a90658d1dd971fdfc918aea3c824aed22
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-04-14 00:11:58 +0200

    Solving Staticfiles_Dir issues

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -147,12 +147,12 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
     # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
+    # STATICFILES_DIRS = (
+    #     os.path.join(PROJECT_ROOT, 'static'),
+    # )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
