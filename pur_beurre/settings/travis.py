2232b5fbc48351b47b655c7d6b12228137daf96c
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-04-13 17:34:43 +0200

    Travis Settings correction typo OS use

--- pur_beurre/settings/travis.py
+++ pur_beurre/settings/travis.py
@@ -6,8 +6,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travis_test',
-        'USER': os.enrivon.get('POSTGRES_USER'),
-        'PASSWORD': os.enrivon.get('POSTGRES_PASS'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASS'),
         'HOST': 'localhost',
         'PORT': '5432',
     },
